e07a871449e32a4ab18006fc3927e4b63a0f2e49
snalini1 <161677034+snalini1@users.noreply.github.com>
2025-07-15 23:53:19 -0400

    Update index.tsx

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -3,11 +3,27 @@ import { Route, Routes, Navigate } from "react-router";
 import TOC from "./TOC";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
+
 export default function Labs() {
   return (
     <div>
       <h1>Labs</h1>
+
+      {/* ✅ Your full name */}
+      <h4>Nalini Singh</h4>
+
+      {/* ✅ GitHub link with correct ID */}
+      <a
+        id="wd-github"
+        href="https://github.com/YOUR_USERNAME/kambaz-react-web-app"
+        target="_blank"
+        rel="noreferrer"
+      >
+        GitHub Repository
+      </a>
+
       <TOC />
+
       <Routes>
         <Route path="/" element={<Navigate to="Lab1" />} />
         <Route path="Lab1" element={<Lab1 />} />
@@ -15,4 +31,5 @@ export default function Labs() {
         <Route path="Lab3/*" element={<Lab3 />} />
       </Routes>
     </div>
-);}
+  );
+}
