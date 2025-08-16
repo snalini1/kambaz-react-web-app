6e49b56def4014aca5d85939e40f07c51ce7c35a
snalini1 <161677034+snalini1@users.noreply.github.com>
2025-08-17 03:54:05 +0400

    Update Status.tsx

--- src/Kambaz/Courses/Home/Status.tsx
+++ src/Kambaz/Courses/Home/Status.tsx
@@ -3,6 +3,12 @@ export default function CourseStatus() {
     <div id="wd-course-status">
       <h2>Course Status</h2>
       <button>Unpublish</button> <button>Publish</button>
-      {/* Complete on your own */}
+      <button>Import existing Content</button>
+      <button>Import from Commons</button>
+      <button>Choose Home page</button>
+      <button>View Course Stream</button>
+      <button>New Announcements</button>
+      <button>New Analytics</button>
       <button>View Course Notifications</button>
-    </div> );}
+    </div>);
+}
