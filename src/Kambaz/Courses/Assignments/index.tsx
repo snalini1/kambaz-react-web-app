6f7b64f4d5141b9c86f64e196596a4bcbc5974a9
snalini1 <161677034+snalini1@users.noreply.github.com>
2025-08-17 04:02:02 +0400

    Update index.tsx

--- src/Kambaz/Courses/Assignments/index.tsx
+++ src/Kambaz/Courses/Assignments/index.tsx
@@ -1,53 +1,41 @@
-import { Link } from "react-router-dom";
-
 export default function Assignments() {
-  return (
-    <div id="wd-assignments">
-      <input
-        placeholder="Search for Assignments"
-        id="wd-search-assignment"
-      />
-      <button id="wd-add-assignment-group">+ Group</button>
-      <button id="wd-add-assignment">+ Assignment</button>
-
-      <h3 id="wd-assignments-title">
-        ASSIGNMENTS 40% of Total <button>+</button>
-      </h3>
-
-      <ul id="wd-assignment-list">
-        <li className="wd-assignment-list-item">
-          <Link
-            to="/Kambaz/Courses/1234/Assignments/123"
-            className="wd-assignment-link"
-          >
-            A1 - ENV + HTML
-          </Link>
-        </li>
-        <li className="wd-assignment-list-item">
-          <Link
-            to="/Kambaz/Courses/1234/Assignments/124"
-            className="wd-assignment-link"
-          >
-            A2 - CSS + Bootstrap
-          </Link>
-        </li>
-        <li className="wd-assignment-list-item">
-          <Link
-            to="/Kambaz/Courses/1234/Assignments/125"
-            className="wd-assignment-link"
-          >
-            A3 - JavaScript + React
-          </Link>
-        </li>
-        <li className="wd-assignment-list-item">
-          <Link
-            to="/Kambaz/Courses/1234/Assignments/126"
-            className="wd-assignment-link"
-          >
-            A4 - State + Props
-          </Link>
-        </li>
-      </ul>
-    </div>
-  );
-}
+    return (
+      <div id="wd-assignments">
+        <input placeholder="Search for Assignments"
+               id="wd-search-assignment" />
+        <button id="wd-add-assignment-group">+ Group</button>
+        <button id="wd-add-assignment">+ Assignment</button>
+        <h3 id="wd-assignments-title">
+          ASSIGNMENTS 40% of Total <button>+</button> </h3>
+        <ul id="wd-assignment-list">
+          <li className="wd-assignment-list-item">
+            <a href="#/Kambaz/Courses/1234/Assignments/123"
+               className="wd-assignment-link" >
+              A1 - ENV + HTML
+            </a> 
+            <div id="wd-p-1">
+              Multiple Modules | Not available until July 4 at 12:00am 
+              | Due July 8 at 11:59pm | 100pts
+            </div>
+            </li>
+          <li className="wd-assignment-list-item">
+          <a href="#/Kambaz/Courses/1234/Assignments/123"
+               className="wd-assignment-link" >
+              A2 - CSS + BOOTSTRAP
+            </a>
+            <div id="wd-p-1">
+            Multiple Modules | Not available until July 6 at 12:00am | Due July 15 at 11:59pm | 100pts
+            </div>
+          </li>
+          <li className="wd-assignment-list-item">
+          <a href="#/Kambaz/Courses/1234/Assignments/123"
+               className="wd-assignment-link" >
+              A3 - JAVASCRIPT + REACT
+            </a>
+            <div id="wd-p-1">
+            Multiple Modules | Not available until July 10 at 12:00am | Due July 22 at 11:59pm | 100pts
+            </div>
+          </li>
+        </ul>
+      </div>
+  );}
