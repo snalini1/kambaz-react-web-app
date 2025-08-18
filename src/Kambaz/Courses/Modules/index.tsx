62afe1ee34d7df5828f170c1663b49615deac38b
snalini1 <161677034+snalini1@users.noreply.github.com>
2025-08-18 13:26:26 +0400

    Update index.tsx in modules in courses in Kamba

--- src/Kambaz/Courses/Modules/index.tsx
+++ src/Kambaz/Courses/Modules/index.tsx
@@ -8,27 +8,70 @@ import { BsGripVertical } from "react-icons/bs";
 export default function Modules() {
   return (
     <div>
-      <ModulesControls /><br /><br /><br />
+      <ModulesControls /><br /><br /><br /><br />
+
       <ListGroup className="rounded-0" id="wd-modules">
         <ListGroup.Item className="wd-module p-0 mb-5 fs-5 border-gray">
           <div className="wd-title p-3 ps-2 bg-secondary">
-            <BsGripVertical className="me-2 fs-3" /> Week 1 <ModuleControlButtons />
+            <BsGripVertical className="me-2 fs-3" /> Week 1: 1/1 - 1/7 <ModuleControlButtons />
           </div>
           <ListGroup className="wd-lessons rounded-0">
             <ListGroup.Item className="wd-lesson p-3 ps-1">
-              <BsGripVertical className="me-2 fs-3" /> LEARNING OBJECTIVES <LessonControlButtons />
+              <BsGripVertical className="me-2 fs-3" /> Lecture 1 (1/1): Introduction <LessonControlButtons />
             </ListGroup.Item>
             <ListGroup.Item className="wd-lesson p-3 ps-1">
-              <BsGripVertical className="me-2 fs-3" /> Introduction to the course <LessonControlButtons />
+              <BsGripVertical className="me-2 fs-3" /> Lecture 2 (1/4): History of Web Development <LessonControlButtons />
             </ListGroup.Item>
             <ListGroup.Item className="wd-lesson p-3 ps-1">
-              <BsGripVertical className="me-2 fs-3" /> Learn what is Web Development <LessonControlButtons />
+              <BsGripVertical className="me-2 fs-3" /> Assignments <LessonControlButtons />
+            </ListGroup.Item>
+            <ListGroup.Item className="wd-lesson p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3" /> Quiz <LessonControlButtons />
             </ListGroup.Item>
           </ListGroup>
         </ListGroup.Item>
-        {/* Add more modules like Week 2 */}
+        <ListGroup.Item className="wd-module p-0 mb-5 fs-5 border-gray">
+          <div className="wd-title p-3 ps-2 bg-secondary">
+            <BsGripVertical className="me-2 fs-3" /> Week 2: 1/7 - 1/14 <ModuleControlButtons />
+          </div>
+          <ListGroup className="wd-lessons rounded-0">
+            <ListGroup.Item className="wd-lesson p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3" /> Lecture 3 (1/8): MERN Stack <LessonControlButtons />
+            </ListGroup.Item>
+            <ListGroup.Item className="wd-lesson p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3" /> Lecture 4 (1/12): Deploying MERN Stack <LessonControlButtons />
+            </ListGroup.Item>
+            <ListGroup.Item className="wd-lesson p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3" /> Assignments <LessonControlButtons />
+            </ListGroup.Item>
+            <ListGroup.Item className="wd-lesson p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3" /> Quiz <LessonControlButtons />
+            </ListGroup.Item>
+          </ListGroup>
+        </ListGroup.Item>
+        <ListGroup.Item className="wd-module p-0 mb-5 fs-5 border-gray">
+          <div className="wd-title p-3 ps-2 bg-secondary">
+            <BsGripVertical className="me-2 fs-3" /> Week 3: 1/14 - 1/21 <ModuleControlButtons />
+          </div>
+          <ListGroup className="wd-lessons rounded-0">
+            <ListGroup.Item className="wd-lesson p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3" /> Lecture 5 (1/15): CyberSecurity: Making your website safe <LessonControlButtons />
+            </ListGroup.Item>
+            <ListGroup.Item className="wd-lesson p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3" /> Lecture 6 (1/19): Really making sure your website is safe <LessonControlButtons />
+            </ListGroup.Item>
+            <ListGroup.Item className="wd-lesson p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3" /> Assignments <LessonControlButtons />
+            </ListGroup.Item>
+            <ListGroup.Item className="wd-lesson p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3" /> Quiz <LessonControlButtons />
+            </ListGroup.Item>
+            <ListGroup.Item className="wd-lesson p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3" /> Exams <LessonControlButtons />
+            </ListGroup.Item>
+          </ListGroup>
+        </ListGroup.Item>
       </ListGroup>
     </div>
   );
 }
-
